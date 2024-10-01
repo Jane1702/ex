5e191f032d73fa3782fb270b0031ec98afe60699
Jane <94185643+Jane1702@users.noreply.github.com>
2024-10-01 11:49:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.embeddings import OpenAIEmbeddings
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 
 st.header("Trang's chatbot 💬 📚")
